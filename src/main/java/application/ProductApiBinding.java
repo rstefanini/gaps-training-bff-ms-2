e7fd50c9b9b90121afa7a7c37e595e8b88ae4e76
stefanir <stefanir@BABZQEHCAF.americas.hpqcorp.net>
2018-02-27 18:47:22 -0300

    MININFUL

--- src/main/java/application/ProductApiBinding.java
+++ src/main/java/application/ProductApiBinding.java
@@ -1,28 +1,23 @@
 package application;
 
-//this is the class that binds to selected services
-import application.model.Product;
-
-import java.util.List;
-import java.util.ArrayList;
-import java.util.HashMap;
-
-import javax.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.springframework.web.multipart.MultipartFile;
+
+//this is the class that binds to selected services
+import application.model.Product;
 
 
 @Component
 public class ProductApiBinding  {
 
-
   public ApiResponseMessage<Product> delete(Long productID) {
       return new ApiResponseMessage<>(HttpStatus.INTERNAL_SERVER_ERROR, "not yet implemented", null);
   }
   public ApiResponseMessage<Product> get(Long productID) {
-      return new ApiResponseMessage<>(HttpStatus.NOT_FOUND, "", null);
+	  Product lfbu = new Product();
+	  lfbu.setIdentifier(productID);
+	  lfbu.setName("Load Factor Based Upgrades");
+      return new ApiResponseMessage<>(HttpStatus.OK, "ok", lfbu);
   }
   public ApiResponseMessage<Product> update(Long productID, String productName) {
       return new ApiResponseMessage<>(HttpStatus.NOT_FOUND, "", null);
---
+++ src/test/java/application/ProductApiBindingTest.java
@@ -0,0 +1,37 @@
+package application;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes=io.swagger.Swagger2SpringBoot.class)
+public class ProductApiBindingTest {
+
+	@Autowired
+	ProductApiBinding productApiBinding;
+	
+	
+	@Test
+	public final void productShouldNotBeNull(){
+	assertNotNull(productApiBinding.get(1L));
+	}
+	
+	@Test
+	public final void testProductsListShouldNotBeNull() {
+	assertNotNull(productApiBinding.get(1L)); 
+	}
+	
+	@Test
+	public final void testProductnameShouldBeLFBU() {
+	assertEquals("Load Factor Based Upgrades", productApiBinding.get(1L).getData().getName()); 
+	}
+
+}
