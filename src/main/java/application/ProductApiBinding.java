20471f9f6fb81b4452d7bad70475a75a25799dd1
Mariano Moreno <mariano.moreno@aa.com>
2018-03-01 15:49:20 -0300

    api integration

--- src/main/java/application/ProductApiBinding.java
+++ src/main/java/application/ProductApiBinding.java
@@ -3,20 +3,26 @@ package application;
 //this is the class that binds to selected services
 import application.model.Product;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class ProductApiBinding {
+
+    @Autowired
+    private PropertiesConfig propertiesConfig;
 
     public ApiResponseMessage<Product> delete(Long productID) {
         return new ApiResponseMessage<>(HttpStatus.INTERNAL_SERVER_ERROR, "not yet implemented", null);
     }
 
     public ApiResponseMessage<Product> get(Long productID) {
-        Product p = new Product();
-        p.setIdentifier(1L);
-        p.setName("First Product");
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Product> churro = restTemplate.getForEntity((propertiesConfig.getProductUrl() + productID), Product.class);
+        Product p = churro.getBody();
         if (productID == p.getIdentifier()) {
             return new ApiResponseMessage<>(HttpStatus.OK, "OK", p);
         } else {
